6a942c4d9524d3759987a3b469db68281ad426c5
aignatiuc <arina.ignatiuc@ee.utm.md>
2019-12-03 12:02:12 +0200

    Merged changes from dev branch

--- app/src/main/java/com/example/takeaction/MainActivity.java
+++ app/src/main/java/com/example/takeaction/MainActivity.java
@@ -1,20 +1,13 @@
 package com.example.takeaction;
-import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import androidx.appcompat.widget.Toolbar;
 import android.os.Bundle;
 
-
 public class MainActivity extends AppCompatActivity {
-    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
     }
 }
