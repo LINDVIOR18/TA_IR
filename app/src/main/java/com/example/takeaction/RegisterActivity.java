a478c9f29888bd571ccc69f65645cd122fe66f2f
mbudeci <Uh4Ck3dme>
2019-12-04 15:23:18 +0200

    Merge with dev branch

--- app/src/main/java/com/example/takeaction/RegisterActivity.java
+++ app/src/main/java/com/example/takeaction/RegisterActivity.java
@@ -19,7 +19,6 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-
 import java.util.Objects;
 
 public class RegisterActivity extends AppCompatActivity {
