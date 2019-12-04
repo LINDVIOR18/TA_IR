6745560a6230fe342d059717be5eb25b39a0a185
aghermanHP <adrian.gherman1@gmail.com>
2019-12-04 10:14:51 +0200

    was seted default marker on Chisinau

--- app/src/main/java/com/example/takeaction/MapsActivity.java
+++ app/src/main/java/com/example/takeaction/MapsActivity.java
@@ -8,6 +8,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -39,9 +40,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        // Add a marker in Chisinau and move the camera
+        LatLng coordinates = new LatLng(47.003670, 28.907089);
+        mMap.addMarker(new MarkerOptions().position(coordinates).title("Marker in Chisianu"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(coordinates));
     }
 }
