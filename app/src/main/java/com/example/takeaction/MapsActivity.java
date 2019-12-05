186257800e420280db0d58c80a1f90eb24fffcda
aghermanHP <adrian.gherman1@gmail.com>
2019-12-05 12:53:17 +0200

    was aded pin imaheView, and onCameraIdle for  geting coordinates

--- app/src/main/java/com/example/takeaction/MapsActivity.java
+++ app/src/main/java/com/example/takeaction/MapsActivity.java
@@ -2,7 +2,11 @@ package com.example.takeaction;
 
 import androidx.fragment.app.FragmentActivity;
 
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -12,18 +16,25 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.IOException;
+import java.util.List;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private GoogleMap.OnCameraIdleListener onCameraIdleListener;
+    private TextView resutText;
+    SupportMapFragment mapFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps);
+        setContentView(R.layout.activity_maps_report_incident);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+                .findFragmentById(R.id.mapReportIncident);
         mapFragment.getMapAsync(this);
+        configureCameraIdle();
     }
 
 
@@ -36,13 +47,33 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+
+    private void configureCameraIdle() {
+        onCameraIdleListener = new GoogleMap.OnCameraIdleListener() {
+            @Override
+            public void onCameraIdle() {
+
+                LatLng latLng = mMap.getCameraPosition().target;
+                Geocoder geocoder = new Geocoder(MapsActivity.this);
+
+                public static void main(){
+                    
+                }
+
+            }
+        };
+    }
+
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        mMap.setOnCameraIdleListener(onCameraIdleListener);
 
         // Add a marker in Chisinau and move the camera
         LatLng coordinates = new LatLng(47.003670, 28.907089);
-        mMap.addMarker(new MarkerOptions().position(coordinates).title("Marker in Chisianu"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(coordinates));
+//        mMap.addMarker(new MarkerOptions().position(coordinates).title("Marker in Chisianu"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(coordinates, 15));
+
     }
 }
