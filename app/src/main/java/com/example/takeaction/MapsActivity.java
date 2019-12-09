333a0bcc02c3fb34ffe7a5720e66f4d67c61fa36
aghermanHP <adrian.gherman1@gmail.com>
2019-12-09 13:25:47 +0200

    was pinner of addres and textView for address

--- app/src/main/java/com/example/takeaction/MapsActivity.java
+++ app/src/main/java/com/example/takeaction/MapsActivity.java
@@ -5,6 +5,7 @@ import androidx.fragment.app.FragmentActivity;
 import android.location.Address;
 import android.location.Geocoder;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.widget.TextView;
 
@@ -12,19 +13,19 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Locale;
+
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    TextView txtLocationAddress;
     private GoogleMap mMap;
     private GoogleMap.OnCameraIdleListener onCameraIdleListener;
-    private TextView resutText;
-    SupportMapFragment mapFragment;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,42 +34,62 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.mapReportIncident);
+        txtLocationAddress = findViewById(R.id.IncidentAddressDeclaration);
+        txtLocationAddress.setEllipsize(TextUtils.TruncateAt.MARQUEE);
+        txtLocationAddress.setSingleLine(true);
+        txtLocationAddress.setMarqueeRepeatLimit(-1);
+        txtLocationAddress.setSelected(true);
         mapFragment.getMapAsync(this);
         configureCameraIdle();
     }
-
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-
-    private void configureCameraIdle() {
-        onCameraIdleListener = new GoogleMap.OnCameraIdleListener() {
-            @Override
-            public void onCameraIdle() {
-
-                LatLng latLng = mMap.getCameraPosition().target;
-                Geocoder geocoder = new Geocoder(MapsActivity.this);
-            }
-        };
-    }
-
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         mMap.setOnCameraIdleListener(onCameraIdleListener);
 
-        // Add a marker in Chisinau and move the camera
-        LatLng coordinates = new LatLng(47.003670, 28.907089);
-//        mMap.addMarker(new MarkerOptions().position(coordinates).title("Marker in Chisianu"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(coordinates, 15));
 
+        // Add a marker in Chisinau and move the camera
+        LatLng coordinates = new LatLng(47.0105, 28.8638);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(coordinates, 15));
+    }
+
+    private void configureCameraIdle() {
+        onCameraIdleListener = new GoogleMap.OnCameraIdleListener() {
+            @Override
+            public void onCameraIdle() {
+                LatLng latLng = mMap.getCameraPosition().target;
+                Log.v("Camera", "lat: " + latLng.latitude + " , long: " + latLng.longitude);
+                getAddressFromLocation(latLng.latitude, latLng.longitude);
+            }
+        };
+    }
+
+    private void getAddressFromLocation(double latitude, double longitude) {
+
+        Geocoder geocoder = new Geocoder(this, Locale.ENGLISH);
+
+
+        try {
+            List<Address>
+             addresses = geocoder.getFromLocation(latitude, longitude, 1);
+
+            if (addresses.size() > 0) {
+                Address fetchedAddress = addresses.get(0);
+                StringBuilder strAddress = new StringBuilder();
+                for (int i = 0; i < fetchedAddress.getMaxAddressLineIndex(); i++) {
+                    strAddress.append(fetchedAddress.getAddressLine(i)).append(" ");
+                }
+
+                txtLocationAddress.setText(strAddress.toString());
+
+            } else {
+                txtLocationAddress.setText("Searching Current Address");
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+           return;
+        }
     }
 }
