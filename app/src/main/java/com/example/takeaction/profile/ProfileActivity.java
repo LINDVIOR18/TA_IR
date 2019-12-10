930ddd83f1b859e590edddad1de0594529c22471
gbusila <Galaxynote10.1>
2019-12-10 10:59:43 +0200

    Minor updates

--- app/src/main/java/com/example/takeaction/profile/ProfileActivity.java
+++ app/src/main/java/com/example/takeaction/profile/ProfileActivity.java
@@ -6,7 +6,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -14,6 +16,11 @@ import android.widget.Toast;
 import com.example.takeaction.PermissionManager;
 import com.example.takeaction.R;
 import com.example.takeaction.cameradialog.CameraDialog;
+
+import java.io.IOException;
+
+import static com.example.takeaction.PermissionManager.CAMERA_PERMISSION_CODE;
+import static com.example.takeaction.PermissionManager.STORAGE_PERMISSION_CODE;
 
 
 public class ProfileActivity extends AppCompatActivity {
@@ -41,7 +48,7 @@ public class ProfileActivity extends AppCompatActivity {
                 permissions,
                 grantResults);
 
-        if (requestCode == PermissionManager.CAMERA_PERMISSION_CODE) {
+        if (requestCode == CAMERA_PERMISSION_CODE) {
             if (grantResults.length > 0
                     && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 Toast.makeText(ProfileActivity.this,
@@ -84,12 +91,27 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == 7 && resultCode == RESULT_OK) {
+        if ((requestCode == CAMERA_PERMISSION_CODE || requestCode == STORAGE_PERMISSION_CODE) && resultCode == RESULT_OK) {
 
             Bitmap bitmap = (Bitmap) data.getExtras().get("data");
 
             imageView.setImageBitmap(bitmap);
         }
+        if (requestCode == STORAGE_PERMISSION_CODE && resultCode == RESULT_OK && data != null && data.getData() != null) {
+
+            Uri uri = data.getData();
+
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+                // Log.d(TAG, String.valueOf(bitmap));
+
+                ImageView imageView = findViewById(R.id.imgV_avatar);
+                imageView.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
     }
 
 }
